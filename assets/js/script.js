d6198e325d273a7316d14566a8fa7d152f65115d
Gabriela Gallardo Reyes <gabs.gallardo@gmail.com>
2024-07-25 15:32:19 -0400

    mejorando el aspecto visual con cositas mas bonis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-    $(".btn-projects").click(function(){
-        $(".projects1").toggle();
-    })
-    $(".btn-projects").click(function(){
-      $(".btn-mas").toggle();
-  })
-
-    $(".btn-mas").dblclick(function(){
-      $(".box3").show();
-    })
-  
     $(".btn-enviar").click(function (){
       alert("Message was sent successfully!")
     })
@@ -18,9 +7,6 @@ $(document).ready(function() {
       $(".offcanvas").hide()
     })
 
-    $(".btn-enviar").click(function(){
-      $("form").validate();
-    })
     
 
 })
