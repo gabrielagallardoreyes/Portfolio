305dcec6daeee8e320b72ce2e1e59138335fcb0e
Gabriela Gallardo Reyes <gabs.gallardo@gmail.com>
2024-07-25 21:02:49 -0400

    Actualización de diseño

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,17 @@ $(document).ready(function() {
       $(".offcanvas").hide()
     })
 
+    $(".fa-lightbulb").mouseenter(function(){
+      $(".nav").css("background-color", "white")
+      $(".nav-text").css("color", "black")
+      $(".fa-lightbulb").css("color","black")
+      $("footer").css("background-color", "white")
+      $("#links").css("color","black")
+      $(".f-iconos").css("color","black")
+      $(".main-box").css("background-color", "white")
+      $("video").hide()
+    })
+
     
 
 })
